f7a6f88a3a9bee8d1e7cec46adb36dd006c8d496
rajatkhdk <rajatkhdk333@gmail.com>
2026-04-22 16:53:55 +0545

    Started dealer info extractor

--- address_json/combine_json.py
+++ address_json/combine_json.py
@@ -1,4 +1,5 @@
 import json
+from collections import defaultdict
 
 with open("provinces.json", encoding="utf-8") as f:
     provinces = json.load(f)
@@ -14,27 +15,49 @@ district_map = {d["district_id"]: d for d in districts}
 
 LOCATION_DB = []
 
+# 1. Add municipality entries
 for loc in locals:
     district = district_map.get(loc["district_id"])
     province = province_map.get(district["province_id"]) if district else None
 
     LOCATION_DB.append({
+        "name": loc["name"].lower(),
+        "type": "municipality",
         "municipality": loc["name"].lower(),
         "district": district["name"].lower() if district else None,
         "province": province["name"].lower() if province else None
     })
 
+# 2. Add district entries separately
+for d in districts:
+    province = province_map.get(d["province_id"])
+
+    LOCATION_DB.append({
+        "name": d["name"].lower(),
+        "type": "district",
+        "municipality": None,
+        "district": d["name"].lower(),
+        "province": province["name"].lower() if province else None
+    })
+
 # print(LOCATION_DB)
 
-LOCATION_LOOKUP = {}
+# Build lookup without overwriting
+LOCATION_LOOKUP = defaultdict(list)
 
 for item in LOCATION_DB:
-    # municipality
-    if item["municipality"]:
-        LOCATION_LOOKUP[item["municipality"]] = item
-    
-    # district
-    if item["district"]:
-        LOCATION_LOOKUP[item["district"]] = item
+    # ALWAYS index by name
+    LOCATION_LOOKUP[item["name"]].append(item)
 
-print(LOCATION_LOOKUP)
+    # # ALSO index by district (important fix)
+    # if item["district"]:
+    #     LOCATION_LOOKUP[item["district"]].append(item)
+
+    # # ALSO index by municipality if exists
+    # if item["municipality"]:
+    #     LOCATION_LOOKUP[item["municipality"]].append(item)
+
+# print(LOCATION_LOOKUP)
+
+with open("address.json", "w", encoding = "utf-8") as f:
+    json.dump(LOCATION_LOOKUP, f, ensure_ascii=False, indent=2)
